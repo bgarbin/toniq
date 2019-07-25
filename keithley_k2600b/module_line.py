bf4a693c3296504cf93fbd6f47d5b916a5fb20fd
qcha41 <31767137+qcha41@users.noreply.github.com>
2019-07-25 19:23:44 +0200

    Update module_line.py

--- keithley_k2600b/module_line.py
+++ keithley_k2600b/module_line.py
@@ -15,6 +15,7 @@ class Line():
         # Initialisation
         self.dev.write(f"smu{self.SLOT}.source.autorangev = smu{self.SLOT}.AUTORANGE_ON")
         self.dev.write(f"smu{self.SLOT}.source.autorangei = smu{self.SLOT}.AUTORANGE_ON")
+        self.dev.query('*OPC?')
     
     def setSafeState(self):
         self.setVoltage(0)
@@ -31,6 +32,7 @@ class Line():
 
     def getResistance(self):
         self.dev.write(f'display.smu{self.SLOT}.measure.func = display.MEASURE_OHMS')
+        self.dev.query('*OPC?')
         return float(self.dev.query(f"print(smu{self.SLOT}.measure.r())"))
     
     
@@ -38,6 +40,7 @@ class Line():
     
     def getPower(self):
         self.dev.write(f'display.smu{self.SLOT}.measure.func = display.MEASURE_WHATTS')
+        self.dev.query('*OPC?')
         return float(self.dev.query(f"print(smu{self.SLOT}.measure.p())"))
 
 
@@ -47,6 +50,7 @@ class Line():
         assert isinstance(float(value),float)
         value = float(value)
         self.dev.write("smu{self.SLOT}.source.limitp = {value}")
+        self.dev.query('*OPC?')
         
     def getPowerCompliance(self):
         return float(self.dev.query(f"print(smu{self.SLOT}.source.limitp)"))
@@ -64,6 +68,7 @@ class Line():
         value = float(value)
         self.dev.write(f"smu{self.SLOT}.source.func = smu{self.SLOT}.OUTPUT_DCAMPS")
         self.dev.write(f"smu{self.SLOT}.source.leveli = {value}")
+        self.dev.query('*OPC?')
 #        if value != 0. and self.getOutputState() is False :
 #            self.setOutputState(True)
 #        if value == 0. and self.getOutputState() is True :
@@ -77,6 +82,7 @@ class Line():
         assert isinstance(float(value),float)
         value = float(value)
         self.dev.write("smu{self.SLOT}.source.limiti = {value}")
+        self.dev.query('*OPC?')
         
     def getCurrentCompliance(self):
         return float(self.dev.query("print(smu{self.SLOT}.source.limiti)"))
@@ -88,6 +94,7 @@ class Line():
     
     def getVoltage(self):
         self.dev.write(f'display.smu{self.SLOT}.measure.func = display.MEASURE_DCVOLTS')
+        self.dev.query('*OPC?')
         return float(self.dev.query(f"print(smu{self.SLOT}.measure.v())"))
     
     def setVoltage(self,value):
@@ -95,6 +102,7 @@ class Line():
         value = float(value)
         self.dev.write(f"smu{self.SLOT}.source.func = smu{self.SLOT}.OUTPUT_DCVOLTS")
         self.dev.write(f"smu{self.SLOT}.source.levelv = {value}")
+        self.dev.query('*OPC?')
 #        if value != 0. and self.getOutputState() is False :
 #            self.setOutputState(True)
 #        if value == 0. and self.getOutputState() is True :
@@ -106,6 +114,7 @@ class Line():
         assert isinstance(float(value),float)
         value = float(value)
         self.dev.write("smu{self.SLOT}.source.limitv = {value}")
+        self.dev.query('*OPC?')
         
     def getVoltageCompliance(self):
         return float(self.dev.query("print(smu{self.SLOT}.source.limitv)"))
@@ -125,6 +134,7 @@ class Line():
             self.dev.write(f"smu{self.SLOT}.source.output = smu{self.SLOT}.OUTPUT_ON")
         else :
             self.dev.write(f"smu{self.SLOT}.source.output = smu{self.SLOT}.OUTPUT_OFF") 
+        self.dev.query('*OPC?')
         
     
             
@@ -136,6 +146,7 @@ class Line():
             self.dev.write(f'smu{self.SLOT}.sense = smu{self.SLOT}.SENSE_REMOTE')
         else :
             self.dev.write(f'smu{self.SLOT}.sense = smu{self.SLOT}.SENSE_LOCAL')  
+        self.dev.query('*OPC?')
 
     def get4wireModeState(self):
         result=int(float(self.dev.query(f"print(smu{self.SLOT}.sense)")))
