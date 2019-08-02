77deced0f99cb65f138ff791b35804eaf60c8298
qcha41 <q.chateiller@gmail.com>
2019-08-02 14:51:03 +0200

    update ilx driver

--- ilxlightwave_ldc3724c/ilxlightwave_ldc3724c.py
+++ ilxlightwave_ldc3724c/ilxlightwave_ldc3724c.py
@@ -7,19 +7,24 @@ Created on Wed Apr  3 20:06:08 2019
 import visa
 import time 
 
+from module_las import LAS
+from module_tec import TEC
+
 ADDRESS = 'ASRL5::INSTR'
 
 class Device():
     
     def __init__(self,address=ADDRESS):
         
-        self.TIMEOUT = 15000 #ms
         
         # Instantiation
         rm = visa.ResourceManager()
         self.controller = rm.open_resource(address)
-        self.controller.timeout = self.TIMEOUT
-        
+        self.controller.baud_rate = 19200
+        self.controller.timeout = 10000
+        self.controller.read_termination='\n'
+        self.controller.write_termination='\n'
+#        
         # Initialisation
         self.write('*CLS')      # Clear status registers
         self.write('DELAY 0')   # User commands not delayed
@@ -34,7 +39,6 @@ class Device():
 
     def query(self,command):
         result = self.controller.query(command)
-        result = result.strip('\n')
         if '=' in result : result = result.split('=')[1]
         try : result = float(result)
         except: pass
@@ -42,6 +46,7 @@ class Device():
     
     def write(self,command):
         self.controller.write(command)
+        time.sleep(0.1) # required by experience
         
         
         
@@ -56,254 +61,6 @@ class Device():
     
 
 
-
-class LAS():
-    
-    def __init__(self,dev):
-        
-        self.dev = dev
-        self.PRECISION = 0.1
-
-
-    
-    
-    def getCurrent(self):
-        self.dev.write('LAS:DIS:LDI')
-        self.dev.query('*OPC?')
-        return float(self.dev.query('LAS:LDI?'))
-
-    def getCurrentSetpoint(self):
-        return float(self.dev.query('LAS:SET:LDI?'))
-        
-    def setCurrent(self,value):
-        assert isinstance(float(value),float)
-        value = float(value)
-        self.dev.write(f'LAS:LDI {value}')
-        self.dev.write('LAS:DIS:SET')
-        self.dev.query('*OPC?')
-        if value > 0 :
-            if self.isEnabled() is False :
-                self.setEnabled(True)
-            self.setMode('ILBW')
-            self.waitForConvergence(self.getCurrent,value)
-        elif value == 0 :
-            if self.isEnabled() is True :
-                self.setEnabled(False)
-
-
-
-
-    def setPower(self,value):
-        assert isinstance(float(value),float)
-        value = float(value)
-        self.dev.write(f'LAS:MDP {value}')
-        self.dev.write('LAS:DIS:SET')
-        self.dev.query('*OPC?')
-        if value > 0 :
-            if self.isEnabled() is False :
-                self.setEnabled(True)
-            self.setMode('MDP')
-            self.waitForConvergence(self.getPower,value)
-        elif value == 0 :
-            if self.isEnabled() is True :
-                self.setEnabled(False)
-
-    def getPower(self):
-        self.dev.write('LAS:DIS:MDP')
-        self.dev.query('*OPC?')
-        return float(self.dev.query('LAS:MDP?'))
-
-    def getPowerSetpoint(self):
-        return float(self.dev.query('LAS:SET:MDP?'))
-    
-    
-    
-    
-    def setEnabled(self,value):
-        assert isinstance(value,bool)
-        self.dev.write(f'LAS:OUT {int(value)}')
-        self.dev.query('*OPC?')
-        if value is True :
-            mode = self.dev.query('LAS:MODE?')
-            if mode.startswith('I'):
-                self.waitForConvergence(self.getCurrent,
-                                        self.getCurrentSetpoint())
-            elif mode.startswith('MD'):
-                self.waitForConvergence(self.getPower,
-                                        self.getPowerSetpoint())
-        
-    def isEnabled(self):
-        return bool(int(self.dev.query('LAS:OUT?')))
-
-    
-    
-    
-    def waitForConvergence(self,func,setPoint):
-        tini = time.time()
-        while True :
-            try : 
-                if abs(func()-setPoint) < self.PRECISION*setPoint :
-                    break
-                else :
-                    time.sleep(0.5)
-            except :
-                pass
-            if time.time() - tini > 5 :
-                break
-            
-            
-            
-            
-    def setMode(self,mode):
-        assert isinstance(mode,str)
-        currMode = self.getMode()
-        enabledMode = self.isEnabled()
-        if currMode != mode :
-            self.dev.write(f'LAS:MODE:{mode}')
-            self.dev.query('*OPC?')
-            self.setEnabled(enabledMode)
-            
-    def getMode(self):
-        return self.dev.query('LAS:MODE?')
-        
-        
-    
-    
-    
     
     
 
-
-
-class TEC():
-    
-    def __init__(self,dev):
-        
-        self.dev = dev
-        self.PRECISION = 0.05
-
-
-
-    def getResistance(self):
-        self.write('TEC:DIS:R')
-        self.dev.query('*OPC?')
-        return float(self.query('TEC:R?'))
-
-
-
-    def setGain(self,value):
-        assert isinstance(int(value),int)
-        value = int(value)
-        self.write(f'TEC:GAIN {value}')
-        self.dev.query('*OPC?')
-        
-    def getGain(self):
-        return int(float(self.query('TEC:GAIN?')))
-   
-    
-
-    
-    def getCurrent(self):
-        self.dev.write('TEC:DIS:ITE')
-        self.dev.query('*OPC?')
-        return float(self.dev.query('TEC:ITE?'))
-
-    def getCurrentSetpoint(self):
-        return float(self.dev.query('TEC:SET:ITE?'))
-        
-    def setCurrent(self,value):
-        assert isinstance(float(value),float)
-        value = float(value)
-        self.dev.write(f'TEC:ITE {value}')
-        self.dev.write('TEC:DIS:SET')
-        self.dev.query('*OPC?')
-        if value > 0 :
-            if self.isEnabled() is False :
-                self.setEnabled(True)
-            self.setMode('ITE')
-            self.waitForConvergence(self.getCurrent,value)
-        elif value == 0 :
-            if self.isEnabled() is True :
-                self.setEnabled(False)
-
-
-
-
-    def setTemperature(self,value):
-        assert isinstance(float(value),float)
-        value = float(value)
-        self.dev.write(f'TEC:T {value}')
-        self.dev.write('TEC:DIS:SET')
-        self.dev.query('*OPC?')
-        if value > 0 :
-            if self.isEnabled() is False :
-                self.setEnabled(True)
-            self.setMode('T')
-            self.waitForConvergence(self.getTemperature,value)
-        elif value == 0 :
-            if self.isEnabled() is True :
-                self.setEnabled(False)
-
-    def getTemperature(self):
-        self.dev.write('TEC:DIS:T')
-        self.dev.query('*OPC?')
-        return float(self.dev.query('TEC:T?'))
-
-    def getTemperatureSetpoint(self):
-        return float(self.dev.query('TEC:SET:T?'))
-    
-    
-
-    
-    
-    def setEnabled(self,value):
-        assert isinstance(value,bool)
-        self.dev.write(f'TEC:OUT {int(value)}')
-        self.dev.query('*OPC?')
-        if value is True :
-            mode = self.dev.query('TEC:MODE?')
-            if mode.startswith('I'):
-                self.waitForConvergence(self.getCurrent,
-                                        self.getCurrentSetpoint())
-            elif mode.startswith('T'):
-                self.waitForConvergence(self.getTemperature,
-                                        self.getTemperatureSetpoint())
-        
-    def isEnabled(self):
-        return bool(int(self.dev.query('TEC:OUT?')))
-    
-    
-    
-    
-    
-    def waitForConvergence(self,func,setPoint):
-        tini = time.time()
-        while True :
-            try : 
-                if abs(func()-setPoint) < self.PRECISION*setPoint :
-                    break
-                else :
-                    time.sleep(0.5)
-            except :
-                pass
-            if time.time() - tini > 5 :
-                break
-            
-            
-            
-            
-    def setMode(self,mode):
-        assert isinstance(mode,str)
-        currMode = self.getMode()
-        enabledMode = self.isEnabled()
-        if currMode != mode :
-            self.dev.write(f'TEC:MODE:{mode}')
-            self.dev.query('*OPC?')
-            self.setEnabled(enabledMode)
-            
-    def getMode(self):
-        return self.dev.query('TEC:MODE?')
-        
-        
-        
-        
