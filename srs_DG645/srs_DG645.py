3d16f3c4a36313325c7ba22ea89f23e5876fb73d
Bruno Garbin <bruno@toniq-04.c2n.u-psud.fr>
2019-08-16 13:42:22 +0200

    bruno: update a few instruments

--- srs_DG645/srs_DG645.py
+++ srs_DG645/srs_DG645.py
@@ -12,133 +12,110 @@ conv = ['T0','T1','A','B','C','D','E','F','G','H']
 conv2 = ['T0','AB','CD','EF','GH']
 
 class Device():
-        def __init__(self,channel= None, query=None,command=None, address=ADDRESS, delay= None, voltage = None, trigger = None, polarity = None, level= None, disp = None):
-            ### Initiate communication ###
-            self.scope = v.Instrument(address)
-       
-            if query:
-                print('\nAnswer to query:',query)
-                rep = self.query(query)
-                print(rep,'\n')
-            elif command:
-                print('\nExecuting command',command)
-                self.write(command)
-                print('\n')
-                
-            if delay:
-                self.ad_delay(channel, delay)
-                    
-            if voltage:
-                #voltage adjust
-                print('Adjust Voltage: ' + channel + ' '+voltage+'V\n')
-                self.write('LAMP'+str(conv2.index(channel))+','+voltage)
-                
-            if trigger: 
-                print('Adjust Trigger: ' +trigger+'Hz\n')
-                self.write('TRAT'+trigger)
-                if disp:
-                    print('Trig Freq     :'+ self.query('TRAT?')+'Hz\n')
-                    disp = False
-            if polarity:
-                print('Adjust Polarity: '+channel+'-'+polarity+'\n')
-                self.write('LPOL'+str(conv2.index(channel))+','+polarity)
-            if level: 
-                print('Adjust Level: '+channel+' '+level+'\n')
-                self.write('LOFF'+str(conv2.index(channel))+','+level)
-                
-            if disp:
-                if (channel == []):
-                    for chan in conv2[1:5]:
-                        self.ch_disp(chan)
-                else: 
-                    self.ch_disp(channel)
-                  
-            sys.exit()
+    def __init__(self,address=ADDRESS):
+        ### Initiate communication ###
+        self.inst = v.Instrument(address)
+
+
+    def get_frequency(self):
+        return self.query('TRAT?')
+    
+    def amplitude(self,channel,amplitude):
+        self.write('LAMP'+str(conv2.index(channel))+','+amplitude)
+    def frequency(self,frequency):
+        self.write('TRAT'+frequency)
+    def polarity(self,channel,polarity):
+        self.write('LPOL'+str(conv2.index(channel))+','+polarity)
+    def offset(self,channel,offset):
+        self.write('LOFF'+str(conv2.index(channel))+','+offset)
+    def display(self,channel):
+        if (channel == []):
+            for chan in conv2[1:5]:
+                self.ch_disp(chan)
+        else: 
+            self.ch_disp(channel)
             
-#########################################
-        # PRINT OUT CODE 
-        def ch_disp(self,channel):
+    ### PRINT OUT CODE 
+    def ch_disp(self,channel):
+        ch1 = str(conv.index(channel[0]))
+        tmpdelay = self.query('DLAY?'+ch1)
+        tmpdelay = tmpdelay.split(',')
+    
+        if len(channel) == 2:
+            ch = str(conv2.index(channel))
+            ch2 = str(conv.index(channel[1]))
+            
+            tmpdelay2 = self.query('DLAY?'+ch2)
+            tmpdelay2 = tmpdelay2.split(',')
+        
+            print('==========CH:'+channel+'==============')
+            print('Level Amplitude  :  '+self.query('LAMP?'+ch)+' V')
+            print('Level Offset     :  '+self.query('LOFF?'+ch)+' V')
+            print('Level Polarity   :  '+self.query('LPOL?'+ch)+'\n')
+            print('Delay           '+channel[0]+':  '+ conv[int(tmpdelay[0])]+tmpdelay[1]+' s')
+            print('Delay           '+channel[1]+':  '+ conv[int(tmpdelay2[0])]+tmpdelay2[1]+' s\n' )
+        else:
+            print('==========CH:'+channel+'==============')
+            print('Delay           '+channel+':  '+ conv[int(tmpdelay[0])]+tmpdelay[1]+' s\n' )
+    
+    #Channel delay code block 
+    def ad_delay(self, channel, delay):
+        if len(channel) == 2:
             ch1 = str(conv.index(channel[0]))
-            tmpdelay = self.query('DLAY?'+ch1)
-            tmpdelay = tmpdelay.split(',')
-        
-            if len(channel) == 2:
-                ch = str(conv2.index(channel))
-                ch2 = str(conv.index(channel[1]))
-                
-                tmpdelay2 = self.query('DLAY?'+ch2)
-                tmpdelay2 = tmpdelay2.split(',')
-          
-                print('==========CH:'+channel+'==============')
-                print('Level Amplitude  :  '+self.query('LAMP?'+ch)+' V')
-                print('Level Offset     :  '+self.query('LOFF?'+ch)+' V')
-                print('Level Polarity   :  '+self.query('LPOL?'+ch)+'\n')
-                print('Delay           '+channel[0]+':  '+ conv[int(tmpdelay[0])]+tmpdelay[1]+' s')
-                print('Delay           '+channel[1]+':  '+ conv[int(tmpdelay2[0])]+tmpdelay2[1]+' s\n' )
-            else:
-                print('==========CH:'+channel+'==============')
-                print('Delay           '+channel+':  '+ conv[int(tmpdelay[0])]+tmpdelay[1]+' s\n' )
-        
-        #Channel delay code block 
-        def ad_delay(self, channel, delay):
-            if len(channel) == 2:
-                ch1 = str(conv.index(channel[0]))
-                ch2 = str(conv.index(channel[1]))
-            else:
-                ch1 = '0'
-                ch2 = str(conv.index(channel))
-                    
-            print('Adjust Delay: ' + channel + ' '+delay+'s\n')
-            self.write('DLAY'+ch2+','+ch1+','+delay,)
-                
+            ch2 = str(conv.index(channel[1]))
+        else:
+            ch1 = '0'
+            ch2 = str(conv.index(channel))
             
-############# BASIC FUNCTIONS#############
-        def query(self, cmd, nbytes=1000000):
-            """Send command 'cmd' and read 'nbytes' bytes as answer."""
-            self.write(cmd+'\n')
-            r = self.read(nbytes)
-            return r
-        def read(self,nbytes=1000000):
-            return self.scope.read(nbytes)
-        def write(self,cmd):
-            self.scope.write(cmd)
-        
+        self.write('DLAY'+ch2+','+ch1+','+delay,)
+            
+    ### BASIC FUNCTIONS
+    def query(self, cmd, nbytes=1000000):
+        """Send command 'cmd' and read 'nbytes' bytes as answer."""
+        self.write(cmd+'\n')
+        r = self.read(nbytes)
+        return r
+    def read(self,nbytes=1000000):
+        return self.inst.read(nbytes)
+    def write(self,cmd):
+        self.inst.write(cmd)
+    def close(self):
+        self.inst.close()
+
         
 if __name__ == '__main__':
 
     usage = """usage: %prog [options] arg
 
                EXAMPLES:
-                  level change : DG645 AB -v 3 
+                  offset change : DG645 AB -o 3     3v level offset on AB
                   delay change : DG645 AB -d 10e-6 
                   delay wrt t0 : DG645 A -d 10e-6
                   trigger      : DG645 -f 1000000    
                   polarity     : DG645 AB -p 1  / DG645 AB -p 0
-                  offset       : DG645 AB -l 2       2v level offset on AB
                """
     parser = OptionParser(usage)
-    parser.add_option("-c", "--command", type="str", dest="com", default=None, help="Set the command to use." )
-    parser.add_option("-q", "--query", type="str", dest="que", default=None, help="Set the query to use." )
+    parser.add_option("-c", "--command", type="str", dest="command", default=None, help="Set the command to use." )
+    parser.add_option("-q", "--query", type="str", dest="query", default=None, help="Set the query to use." )
     parser.add_option("-d", "--delay", type="str", dest="delay", default=None, help="Set the delay (s)")
-    parser.add_option("-x", "--display", action = "store_true", dest ="disp", default=False, help="disp")
-    parser.add_option("-a", "--voltage", type="str", dest="voltage", default=None, help="Set the amplitude (V)")
-    parser.add_option("-f", "--freq", type="str", dest="freq", default=None, help="Set the trigger (Hz)")
-    parser.add_option("-p", "--polarity", type="str", dest="pol", default=None, help="Set the level polarity if 1, then up if 0, then down")
-    parser.add_option("-l", "--level", type="str", dest="level", default=None, help="Set the level")
+    parser.add_option("-x", "--display", action = "store_true", dest ="display", default=False, help="disp")
+    parser.add_option("-a", "--amplitude", type="str", dest="amplitude", default=None, help="Set the amplitude (V)")
+    parser.add_option("-f", "--frequency", type="str", dest="frequency", default=None, help="Set the frequency (Hz)")
+    parser.add_option("-p", "--polarity", type="str", dest="polarity", default=None, help="Set the level polarity if 1, then up if 0, then down")
+    parser.add_option("-o", "--offset", type="str", dest="offset", default=None, help="Set the offset")
     parser.add_option("-i", "--address", type="string", dest="address", default=ADDRESS, help="Set ip address" )
-    # parser.add_option("-b", "--burst",type="str", dest="level",deafult= None, help="burst options")
     (options, args) = parser.parse_args()
 
     chan = []
-    #Errors for options that do not require a channel input
+    # Errors for options that do not require a channel input
     # command, query, display, 
-    # voltage, freq, polarity, level
+    # amplitude, freq, polarity, level
 
-    if (options.voltage) or (options.delay) or (options.pol) or (options.level):
+    if (options.amplitude) or (options.delay) or (options.pol) or (options.offset):
         if (len(args) == 0):
             print('\nYou must provide at least one edge\n')
             sys.exit()
-        if (options.pol) or (options.voltage) or (options.level):
+        if (options.pol) or (options.amplitude) or (options.offset):
             if (len(args[0])==1):
                 print('\nYou must provide a channel')
                 sys.exit()
@@ -147,13 +124,39 @@ if __name__ == '__main__':
         else:       
             print('\nYou must provide a channel or edge')
             sys.exit()
-    if (options.disp) and (len(args) !=0):
+    if (options.display) and (len(args) !=0):
         if (args[0] in conv) or (args[0] in conv2):
             chan = args[0].upper() 
         else:       
             print('\nYou must provide a channel or edge')
             sys.exit()
     
-
+    
     ### Start the talker ###
-    Device(channel= chan, query=options.que,command=options.com,address=options.address,delay=options.delay,voltage= options.voltage, trigger = options.freq, polarity= options.pol,level = options.level, disp = options.disp)
+    I = Device(address=options.address)
+    
+    if options.query:
+        print('\nAnswer to query:',options.query)
+        rep = I.query(options.query)
+        print(rep,'\n')
+    elif options.command:
+        print('\nExecuting command',options.command)
+        I.write(options.command)
+        print('\n')
+        
+    if options.delay:
+        I.ad_delay(chan, options.delay)
+    if options.amplitude:
+        I.amplitude(chan, options.amplitude)
+    if options.frequency: 
+        I.frequency(options.frequency)
+    if options.polarity:
+        I.polarity(chan,options.polarity)
+    if options.offset: 
+        I.offset(chan,options.offset)
+    if options.display:
+        I.display(chan)
+    
+    sys.exit()
+    
+
--- thorlabs_ITC4001/thorlabs_ITC4001.py
+++ thorlabs_ITC4001/thorlabs_ITC4001.py
@@ -9,39 +9,26 @@ import sys
 ADDRESS = 'USB0::4883::32842::M00248997::INSTR'
 
 class Device():
-    def __init__(self,address=ADDRESS,query=None,command=None,amplitude=None):
+    def __init__(self,address=ADDRESS):
         ### Initiate communication ###
-        rm = v.ResourceManager('@py')
-        self.thorlabs = rm.get_instrument(address)
+        rm = v.ResourceManager()
+        self.inst = rm.get_instrument(address)
 
-        ### Basic communications ###
-        if query:
-            print('\nAnswer to query:',query)
-            rep = self.query(query)
-            print(rep,'\n')
-            sys.exit()
-        elif command:
-            self.command = command
-            print('\nExecuting command',command)
-            self.thorlabs.write(command)
-            print('\n')
-            sys.exit()
-
-        ### change the value of the current ###
-        if amplitude or amplitude==0:
-            self.amplitude(amplitude)
 
     def amplitude(self,amplitude):
-        self.thorlabs.write('SOUR:CURR %f\n' %amplitude)
+        self.write('SOUR:CURR %f\n' %amplitude)
         print('\nSetting current to: ',amplitude,'V\n')
             
     def query(self, cmd):
-        self.thorlabs.write(cmd+'\n')
+        self.write(cmd)
         r = self.read()
         return r
-    
     def read(self):
-        return self.thorlabs.read()
+        return self.inst.read()
+    def write(self,cmd):
+        self.inst.write(cmd+'\n')
+    def close(self):
+        self.inst.close()
         
 if __name__=="__main__":
     
@@ -55,13 +42,31 @@ if __name__=="__main__":
 
                """
     parser = OptionParser(usage)
-    parser.add_option("-c", "--command", type="str", dest="com", default=None, help="Set the command to use." )
-    parser.add_option("-q", "--query", type="str", dest="que", default=None, help="Set the query to use." )
+    parser.add_option("-c", "--command", type="str", dest="command", default=None, help="Set the command to use." )
+    parser.add_option("-q", "--query", type="str", dest="query", default=None, help="Set the query to use." )
     parser.add_option("-a", "--amplitude", type="float", dest="amplitude", default=None, help="Set the pumping current value")
     parser.add_option("-i", "--address", type="str", dest="address", default=ADDRESS, help="Set the USB VISA address to use for the communication")
     (options, args) = parser.parse_args()
     
     
     ### Call the class with arguments ###
-    Device(address=options.address,query=options.que,command=options.com,amplitude=options.amplitude)
+    I = Device(address=options.address)
 
+    ### Basic communications ###
+    if options.query:
+        print('\nAnswer to query:',options.query)
+        rep = I.query(options.query)
+        print(rep,'\n')
+        I.close();sys.exit()
+    elif options.command:
+        print('\nExecuting command',options.command)
+        I.write(options.command)
+        print('\n')
+        I.close();sys.exit()
+
+    ### change the value of the current ###
+    if options.amplitude or options.amplitude==0:
+        I.amplitude(options.amplitude)
+        
+    I.close()
+    sys.exit()
