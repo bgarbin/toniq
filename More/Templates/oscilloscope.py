a8d6fbd1cfd049a6e06f6ca10bb38c13e2456dd8
bruno066 <bruno.garbin.etu@gmail.com>
2019-09-27 16:27:43 +0200

    u

--- More/Templates/oscilloscope.py
+++ More/Templates/oscilloscope.py
@@ -33,7 +33,7 @@ class Device_TCPIP():
         self.inst.write(commande,length=self.length)
 
 
-class Device_VX11(): en haut
+class Device_VXI11():
     def __init__(self, address, **kwargs):
         import vxi11 as v
     
@@ -59,8 +59,8 @@ class Device():
               
         self.nb_channels = nb_channels
         
-        for i in xrange(1,len(self.nb_channels)+1):
-            setattr(self,f'channel{i}',Channel(self,i))
+        #for i in xrange(1,len(self.nb_channels)+1):
+            #setattr(self,f'channel{i}',Channel(self,i))
     
     
     ### User utilities
@@ -70,24 +70,24 @@ class Device():
         self.stop()
         self.is_stopped()
         if channels == []: channels = list(xrange(1,self.nb_channels+1))
-        for i in channels():
-            if not(getattr(self,f'channel{i}').is_active()): continue
-            else:
-                # WARNING Do we warn the user he is stupid?
-            data = getattr(self,f'channel{i}').get_data()
+        #for i in channels():
+            #if not(getattr(self,f'channel{i}').is_active()): continue
+            #else:
+                ## WARNING Do we warn the user he is stupid?
+            #data = getattr(self,f'channel{i}').get_data()
         self.set_previous_trigger_state(previous_trigger_state)
         #return pandas.DataFrame
         
-    def save_all_channels(self):
-        for i in self.active_channels()
-            getattr(self,f'channel{i}').save_data()
+    #def save_all_channels(self):
+        #for i in self.active_channels()
+            #getattr(self,f'channel{i}').save_data()
         
     ### Trigger functions
     def single(self):
-        
+        pass
     def stop(self):
-        
-    def is_stopped(self)
+        pass
+    def is_stopped(self):
         while self.query('TRMD?') != 'TRMD STOP':    # typical example
             time.sleep(0.05)
         return True
@@ -95,7 +95,7 @@ class Device():
         return str(previous_trigger_state)
         
     def set_previous_trigger_state(self):
-        
+        pass
         
     ### Cross-channel settings 
     def set_encoding(self):
@@ -114,11 +114,11 @@ class Channel():
         
     def get_data(self):
     
-        if self.option is True :
-            min= self
-            max =...
+        #if self.option is True :
+            #min= self
+            #max =...
             
-        data = 
+        #data = 
         
         return bytes
         
@@ -126,7 +126,7 @@ class Channel():
     def get_log_data(self):
         return str
     def save_data(self):
-        
+        pass
     def get_data_numerical(self):
         return array_of_float
     def save_data_numerical(self):
@@ -142,23 +142,21 @@ class Channel():
        
     
     def set_autoscale_enabled(bool):
-        self.autoscale = 
-    def is_autoscable_enabled()
+        #self.autoscale = 
+        pass
+    def is_autoscable_enabled():
         return bool
-    
-    def do_autoscale()
+    def do_autoscale():
+        pass
        
-    
     def is_active():
-        
+        pass
 
 
 
-
-        
-        
 if __name__ == '__main__':
     from optparse import OptionParser
+    import inspect
     
     usage = """usage: %prog [options] arg
 
@@ -169,41 +167,31 @@ if __name__ == '__main__':
     parser = OptionParser(usage)
     parser.add_option("-c", "--command", type="str", dest="com", default=None, help="Set the command to use." )
     parser.add_option("-q", "--query", type="str", dest="que", default=None, help="Set the query to use." )
-    parser.add_option("-i", "--address", type="string", dest="address", default='192.168.0.4', help="Set ip address" )
+    parser.add_option("-i", "--address", type="string", dest="address", default='192.168.0.4', help="Set ip address." )
+    parser.add_option("-l", "--link", type="string", dest="link", default='VXI11', help="Set the connection type." )
     (options, args) = parser.parse_args()
     
     ### Compute channels to acquire ###
-    if (len(args) == 0) and (options.com is None) and (options.que is None):
-        print('\nYou must provide at least one channel\n')
-        sys.exit()
-    elif len(args) == 1:
-        chan = []
-        temp_chan = args[0].split(',')                  # Is there a coma?
-        for i in range(len(temp_chan)):
-            chan.append('C' + temp_chan[i])
-    else:
-        chan = []
-        for i in range(len(args)):
-            chan.append('C' + str(args[i]))
-    print('Channel(s):   ', chan)
-    ####################################
-    ### Compute channels for spe_mode to acquire ###
-    if options.spe_mode:
-        if len(options.spe_mode) > 1:
-            spe_mode       = eval(options.spe_mode)[0]
-            spe_mode_chans = eval(options.spe_mode)[1:]
-            chan_spe = []
-            for i in range(len(spe_mode_chans)):
-                chan_spe.append('C'+str(spe_mode_chans[i]))
-            print('Special mode Channel(s):   ', chan_spe)
-        else:
-            chan_spe = chan
-            spe_mode = eval(options.spe_mode)
-            print('Special mode Channel(s):   ', chan_spe)
-    else:
-        chan_spe = None
-        spe_mode = options.spe_mode
+    #if (len(args) == 0) and (options.com is None) and (options.que is None):
+        #print('\nYou must provide at least one channel\n')
+        #sys.exit()
+    #elif len(args) == 1:
+        #chan = []
+        #temp_chan = args[0].split(',')                  # Is there a coma?
+        #for i in range(len(temp_chan)):
+            #chan.append('C' + temp_chan[i])
+    #else:
+        #chan = []
+        #for i in range(len(args)):
+            #chan.append('C' + str(args[i]))
+    #print('Channel(s):   ', chan)
     ####################################
     
     ### Start the talker ###
-    Device()
+    classes = [name for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass) if obj.__module__ is __name__]
+    assert 'Device_'+options.link in classes , "Not in " + str([a for a in classes if a.startwith('Device_')])
+    #print(__name__,__module__)
+    Device_LINK = getattr(__name__,'Device_'+options.link)
+    I = Device_LINK()
+    
+    
